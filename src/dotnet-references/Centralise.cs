71e9fbbdad77c2de88c04d5f786e807c96d233d1
benmccallum <ben.mccallum@live.com.au>
2024-03-18 19:51:21 +1000

    feat: Update the project files and create a pkg props file

--- src/dotnet-references/Centralise.cs
+++ src/dotnet-references/Centralise.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
 using static BenMcCallum.DotNet.References.Common;
 
 namespace BenMcCallum.DotNet.References;
@@ -58,27 +62,61 @@ public static class Centralise
 
         // For all .csproj files that are referenced by the .sln file,
         // pull out nuget package references (name, versions)
-        var packageReferences = new Dictionary<string, HashSet<string>>();
+        // { 
+        //   "SomePackage": {
+        //     "1.2.3": [ element1, element2 ],
+        //     "1.2.4": []
+        //   },
+        //   /...
+        // 
+        var packageReferences = new Dictionary<string, Dictionary<string, List<PackageReference>>>();
+
         var matches = SlnFileCsProjRegex.Matches(slnFileContents);
         foreach (Match match in matches)
         {
             var csProjFileName = ExtractCsProjName(match.Value);
             var csProjFilePath = FindCsProjFilePath(csProjFilePaths, csProjFileName);
 
-            var csProjFileContents = File.ReadAllText(csProjFilePath);
+            // TODO: Explore Microsoft.Build.Evaluation instead
+            // which might make modifying the project and saving it as before easier
 
-            var packageMatches = PackageReferenceRegex.Matches(csProjFileContents);
-            foreach (Match packageMatch in packageMatches)
+            XDocument xmlDoc = XDocument.Load(csProjFilePath);
+            foreach (var xPackageRef in xmlDoc.Descendants("PackageReference"))
             {
-                var packageName = packageMatch.Groups["name"].Value;
-                var packageVersion = packageMatch.Groups["version"].Value;
+                var packageName = xPackageRef.Attribute("Include")?.Value
+                    ?? xPackageRef.Descendants("Include").SingleOrDefault()?.Value
+                    ?? throw new Exception($"Couldn't find package name in {csProjFilePath}");
                 if (!packageReferences.TryGetValue(packageName, out var packageVersions))
                 {
-                    packageVersions = packageReferences[packageName] = new HashSet<string>();
+                    packageVersions = packageReferences[packageName] 
+                        = new Dictionary<string, List<PackageReference>>();
                 }
-                packageVersions.Add(packageVersion);
+
+                var packageVersion = xPackageRef.Attribute("Version")?.Value
+                    ?? xPackageRef.Descendants("Version").SingleOrDefault()?.Value
+                    ?? throw new Exception(
+                        $"Couldn't find package version for {packageName} in {csProjFilePath}");
+                if (!packageVersions.TryGetValue(packageVersion, out var xElements))
+                {
+                    xElements = packageVersions[packageVersion]
+                        = new List<PackageReference>();
+                }
+                xElements.Add(new(xPackageRef, csProjFilePath));
             }
         }
+
+        // Build up a Directory.Packages.props file
+        // (or replace whatever one is there)
+        // https://learn.microsoft.com/en-us/nuget/consume-packages/Central-Package-Management
+        var xProject = new XElement("Project");
+        var xDirectoryPackageProps = new XDocument(xProject);
+        var xPropertyGroup = new XElement("PropertyGroup");
+        xProject.Add(xPropertyGroup);
+        xPropertyGroup.Add(new XElement("ManagePackageVersionsCentrally", "true"));
+        var xItemGroup = new XElement("ItemGroup");
+        xProject.Add(xItemGroup);
+
+        var modifiedPackageReferences = new HashSet<PackageReference>();
 
         // Output helpful XML for those that can be immediately centralised
         var singleVersionedPackages = packageReferences
@@ -86,21 +124,92 @@ public static class Centralise
             .OrderBy(pr => pr.Key)
             .ToArray();
 
-        var sb = new StringBuilder(
-            $"{singleVersionedPackages.Length} packages have a consistent version for central versioning." +
-            $"The below block can be copied into your central Directory.Packages.props file per " +
-            $"https://learn.microsoft.com/en-us/nuget/consume-packages/Central-Package-Management");
-        foreach (var package in singleVersionedPackages) 
+        foreach (var singleVersionedPackage in singleVersionedPackages)
         {
-            sb.AppendLine($"<PackageVersion Include=\"{package.Key}\" Version=\"{package.Value.Single()}\" />");
+            var versionWithElementsKvp = singleVersionedPackage.Value.Single();
+            var packageName = singleVersionedPackage.Key;
+            var packageVersion = versionWithElementsKvp.Key;
+
+            var xPackageReference = new XElement("PackageReference");
+            xPackageReference.SetAttributeValue("Include", packageName);
+            xPackageReference.SetAttributeValue("Version",  packageVersion);
+            xItemGroup.Add(xPackageReference);
+
+            foreach (var packageRef in versionWithElementsKvp.Value)
+            {
+                var versionAttr = packageRef.Element.Attribute("Version");
+                if (versionAttr is not null)
+                {
+                    versionAttr.Remove();
+                }
+                else
+                {
+                    var versionChildElement = packageRef.Element
+                        .Descendants("Version")
+                        .SingleOrDefault();
+                    if (versionChildElement is not null)
+                    {
+                        versionChildElement.Remove();
+                    }
+                }
+
+                modifiedPackageReferences.Add(packageRef);
+            }
+
+            Console.WriteLine($"Centralised {packageName} @ {packageVersion}.");
         }
-        Console.WriteLine(sb.ToString());
-        Console.WriteLine();
+
+        var directoryPackagePropsFilePath = Path.Combine(
+            Path.GetDirectoryName(slnFilePath)!, 
+            "Directory.Packages.props");
+        Console.WriteLine($"Saving {directoryPackagePropsFilePath}.");
+        xProject.Document!.Save(directoryPackagePropsFilePath);
+
+        foreach (var modifiedPackageRef in modifiedPackageReferences)
+        {
+            Console.WriteLine($"Saving changes to {modifiedPackageRef.CsProjFilePath}");
+
+            modifiedPackageRef.Element.Document!.Save(
+                modifiedPackageRef.CsProjFilePath);
+
+            var sb = new StringBuilder();
+            foreach (var line in File.ReadAllLines(modifiedPackageRef.CsProjFilePath).Skip(1))
+            {
+                if (line.StartsWith("<Project") ||
+                    line.StartsWith("  </"))
+                {
+                    sb.AppendLine(line);
+                    sb.AppendLine();
+                }
+                else
+                {
+                    sb.AppendLine(line);
+                }
+            }
+
+            File.WriteAllText(modifiedPackageRef.CsProjFilePath, sb.ToString());
+        }
+
+        //var sb = new StringBuilder(
+        //    $"{singleVersionedPackages.Length} packages have a consistent version for central versioning." +
+        //    $"The below block can be copied into your central Directory.Packages.props file per " +
+        //    $"https://learn.microsoft.com/en-us/nuget/consume-packages/Central-Package-Management");
+        //foreach (var package in singleVersionedPackages) 
+        //{
+        //    sb.AppendLine($"<PackageVersion Include=\"{package.Key}\" Version=\"{package.Value.Single()}\" />");
+        //}
+        //Console.WriteLine(sb.ToString());
+        //Console.WriteLine();
 
         // Output helpful JSON for those that are trickier to centralise
         var multiVersionedPackages = packageReferences
             .Where(pr => pr.Value.Count != 1)
             .OrderBy(pr => pr.Key)
+            .Select(kvp => new
+            {
+                Package = kvp.Key,
+                Versions = kvp.Value.Keys.ToArray()
+            })
             .ToArray();
         
         Console.WriteLine(
@@ -108,4 +217,6 @@ public static class Centralise
         Console.WriteLine(JsonSerializer.Serialize(multiVersionedPackages, _serializerOptions));
         Console.WriteLine();
     }
+
+    private record PackageReference(XElement Element, string CsProjFilePath);
 }
